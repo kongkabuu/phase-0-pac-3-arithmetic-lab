d5aa38fff61c4ab5debefd85a9b8a486158c189d
paul kabuu <kongkabuu9@gmail.com>
2023-03-09 22:49:05 +0300

    assingment done

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b){
+return a+b;
+
+}
+
+function subtract(a,b){
+return a-b;
+}
+
+function multiply(a,b){
+ return a*b;
+
+}
+function divide(a,b){
+return a/b;
+}
+function increment(x){
+
+    return x+=1;
+}
+function decrement(x){
+
+    return x-=1;
+}
+
+function makeInt(x){
+
+    return parseInt(x,10);
+}
+
+function preserveDecimal(x){
+
+    return parseFloat(x);
+}
